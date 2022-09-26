49bac61fc60cb62489148fa7799fded921f07306
Caisr <madaocai@gmail.com>
2022-09-26 14:46:46 +0800

    style: 删除测试代码

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App: React.FC = () => (<div>Hello</div>);
+const App: React.FC = () => (<div>oh my electron!</div>);
 const root = createRoot(document.querySelector('#app')!);
 root.render(<App />);
